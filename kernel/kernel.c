e357614898bc7caf97b9cda13c029fe26b098dc7
abhis_000 <maverick@ubuntu.ubuntu-domain>
2013-08-31 02:36:09 -0700

    Added newline support to terminal

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -1,13 +1,17 @@
-#if !defined(__cplusplus)
-#include <stdbool.h> /* C doesn't have booleans by default. */
-#endif
+#include <stdbool.h>
 #include <stddef.h>
 #include <stdint.h>
+
+#define VGA_ADDRESS 0xB8000
+
+static const size_t VGA_WIDTH  = 80;
+static const size_t VGA_HEIGHT = 24;
  
-/* Check if the compiler thinks if we are targeting the wrong operating system. */
-#if defined(__linux__)
-#error "You are not using a cross-compiler, you will most certainly run into trouble"
-#endif
+static size_t    terminal_row;
+static size_t    terminal_column;
+static uint8_t   terminal_color;
+static uint16_t* terminal_buffer;
+
  
 /* Hardware text mode color constants. */
 enum vga_color
@@ -45,25 +49,21 @@ uint16_t make_vgaentry(char c, uint8_t color)
 size_t strlen(const char* str)
 {
 	size_t ret = 0;
-	while ( str[ret] != 0 )
+	
+	while (str[ret] != 0)
+	{
 		ret++;
+	}
+	
 	return ret;
 }
- 
-static const size_t VGA_WIDTH = 80;
-static const size_t VGA_HEIGHT = 24;
- 
-size_t terminal_row;
-size_t terminal_column;
-uint8_t terminal_color;
-uint16_t* terminal_buffer;
  
 void terminal_initialize()
 {
 	terminal_row = 0;
 	terminal_column = 0;
 	terminal_color = make_color(COLOR_LIGHT_GREY, COLOR_BLACK);
-	terminal_buffer = (uint16_t*) 0xB8000;
+	terminal_buffer = (uint16_t*) VGA_ADDRESS;
 	for ( size_t y = 0; y < VGA_HEIGHT; y++ )
 	{
 		for ( size_t x = 0; x < VGA_WIDTH; x++ )
@@ -87,8 +87,7 @@ void terminal_putentryat(char c, uint8_t color, size_t x, size_t y)
  
 void terminal_putchar(char c)
 {
-	terminal_putentryat(c, terminal_color, terminal_column, terminal_row);
-	if ( ++terminal_column == VGA_WIDTH )
+	if ( c == '\n' )
 	{
 		terminal_column = 0;
 		if ( ++terminal_row == VGA_HEIGHT )
@@ -96,23 +95,35 @@ void terminal_putchar(char c)
 			terminal_row = 0;
 		}
 	}
+	else
+	{
+		terminal_putentryat(c, terminal_color, terminal_column, terminal_row);
+		if ( ++terminal_column == VGA_WIDTH )
+		{
+			terminal_column = 0;
+			if ( ++terminal_row == VGA_HEIGHT )
+			{
+				terminal_row = 0;
+			}
+		}
+	}
 }
  
 void terminal_writestring(const char* data)
 {
 	size_t datalen = strlen(data);
+
 	for ( size_t i = 0; i < datalen; i++ )
+	{
 		terminal_putchar(data[i]);
+	}
 }
  
-#if defined(__cplusplus)
-extern "C" /* Use C linkage for kernel_main. */
-#endif
 void kernel_main()
 {
 	terminal_initialize();
 	/* Since there is no support for newlines in terminal_putchar yet, \n will
 	   produce some VGA specific character instead. This is normal. */
-	terminal_writestring("Hello, kernel World!\n");
+	terminal_writestring("Hello,World!\nFrom Kernel Land\n");
 }
 
